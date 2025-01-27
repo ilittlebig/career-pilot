03fbba8f985b93e6c55df584561296cf0220352f
Elias Sjödin <elias@amsystem.com>
2025-01-27 18:06:10 +0100

    style: navbar to center navigation buttons

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -9,29 +9,36 @@
 </script>
 
 <nav class="flex justify-center sticky top-0 p-4 h-[73px] bg-white w-full border-b z-10">
-	<div class="flex items-center justify-between w-full max-w-6xl">
-		<a href="/" class="font-logo text-xl font-bold text-primary">
+	<div class="flex items-center justify-between w-full max-w-6xl relative">
+		<a href="/" class="font-logo text-xl font-bold text-primary z-10">
 			Logo
 		</a>
-		<ul class="flex items-center gap-4">
-			<li>
-				<a href="/" class="text-sm text-muted-foreground hover:text-primary">Home</a>
-			</li>
-			<li>
-				<a href="/" class="text-sm text-muted-foreground hover:text-primary">Portfolio</a>
-			</li>
-			{#if authenticated}
-				<div class="flex items-center gap-x-2">
-					<Button href="/dashboard">
-						Dashboard
-					</Button>
-					<UserMenu />
-				</div>
-			{:else}
+		<ul class="absolute w-full pointer-events-none">
+			<div class="flex justify-center items-center gap-x-4 pointer-events-auto">
+				<li>
+					<a href="/" class="text-sm text-muted-foreground hover:text-primary">Home</a>
+				</li>
+				<li>
+					<a href="/" class="text-sm text-muted-foreground hover:text-primary">Portfolio</a>
+				</li>
+			</div>
+		</ul>
+		{#if authenticated}
+			<div class="flex items-center gap-x-2 z-10">
+				<Button href="/dashboard">
+					Dashboard
+				</Button>
+				<UserMenu />
+			</div>
+		{:else}
+			<div class="flex items-center gap-x-2 z-10">
+				<Button href="/login" variant="outline">
+					Login
+				</Button>
 				<Button href="/sign-up">
 					Get Started
 				</Button>
-			{/if}
-		</ul>
+			</div>
+		{/if}
 	</div>
 </nav>
