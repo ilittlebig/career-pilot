515484ee896b3882bc187c44756dcd0a66255d14
Elias Sjödin <elias@amsystem.com>
2025-02-20 20:29:45 +0100

    feat: add custom 404 error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { House } from "lucide-svelte";
+	import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="flex flex-col gap-y-4 items-center justify-center h-screen">
+	<img src="/404.svg" class="w-[450px] h-[450px] object-contain" alt="404" />
+	<h2 class="font-semibold text-2xl max-w-xl text-center">
+		They say all roads lead to home, but this one seems to be lost in the cheese maze. 🧀
+	</h2>
+	<Button href="/" size="lg">
+		<House />
+		Take me home
+	</Button>
+</div>
